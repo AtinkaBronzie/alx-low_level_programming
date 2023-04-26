83492b3e85703c4153301f772c29a33035d47216
AtinkaBronzie <jeffreyhansons@gmail.com>
2023-04-26 06:42:22 -0700

    Task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,6 +8,12 @@
  * @a: The first number
  * @b: The second number
  */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 
 int op_add(int a, int b)
 {
